4f95e1aeb60509c6ce3c6e0703146bdfeb78cc99
Brundex <puodziunasb@gmail.com>
2025-05-29 22:22:32 -0300

    Create models and set categorie choices

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -2,13 +2,46 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # AGREGAR FIELDS Y TYPES
+
 class User(AbstractUser):
-    pass
+    watchlist = models.ManyToManyField("Listing", blank=True, related_name="watching")
 class Listing(models.Model):
-    pass
+    CATEGORY_CHOICES = {
+        "Motors": "Motors",
+        "Electronics": "Electronics",
+        "Clothing": "Clothing & Accesories",
+        "Furniture": "Furniture",
+        "Sports": "Sporting Goods",
+        "Other": "Other"        
+    }
+    # Core Details
+    owner = models.ForeignKey("User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=32)
+    description = models.CharField(max_length=64)
+    category = models.CharField(max_length=32, choices=CATEGORY_CHOICES)
+    img_url = models.URLField(blank=True, null=True)
+    
+    # Bidding
+    starting_bid = models.DecimalField(max_digits=9, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=9, decimal_places=9, default=0)
+    
+    # State
+    is_open = models.BooleanField(default=True)
+    
+    
+    
 
 class Bid(models.Model):
-    pass
+    # References
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    listing = models.ForeignKey("Listing", on_delete=models.CASCADE)
+    
+    # Bid values
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
 
 class Comment(models.Model):
-    pass
+    listing = models.ForeignKey("Listing", on_delete=models.CASCADE)
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
